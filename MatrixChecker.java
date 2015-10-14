d9c2672994904f1bb571a8c0960d1746c3df01bd
unknown <sbaird02@CSPC121.dcs.bbk.ac.uk>
2015-10-14 16:51:07 +0100

    Symmetry updates

--- MatrixChecker.java
+++ MatrixChecker.java
@@ -11,7 +11,7 @@ public class MatrixChecker{
 			return false;
 		}
 		for(int i = 0; i < arg.length; i++) {
-			if ()arg[0] != arg[arg.length-1-i]) {
+			if (arg[i] != arg[arg.length-(1+i)]) {
 				return false;
 			}
 		}
@@ -20,9 +20,26 @@ public class MatrixChecker{
 	
 	public boolean isSymmetrical(int[][] arg) {
 		
+		// Assumes array matrix is n x n
+		if (arg == null) {
+			return false;
+		}
+		if (arg.length == 0) {
+			return false;
+		}
+		for(int i = 0; i < arg.length; i++) {
+			
+			for (int j = 0; j < arg[i].length; j++) {
+				
+				if (arg[i][j] != arg[j][i]) {
+					return false;
+				}
+			}			
+		}
+		return true;
 	}
 	
 	public boolean isTriangular(int[][] arg) {
-		
+		return false;
 	}
 }
--- MatrixRunner.java
+++ MatrixRunner.java
@@ -1,6 +1,10 @@
 public class MatrixRunner {
 
-
+	private final int[] symmetricArray = {1,2,3,4,5,4,3,2,1};
+	private final int[] asymmetricArray = {1,2,3,4,5,6,7,8,9};
+	private final int[] zeroesArray = new int[9];
+//	private final int[] zeroesArray = {0,0,0,0,0,0,0,0,0};
+	
 	public static void main(String[] args) {
 	
 //		new MatrixRunner().testConstructor();
@@ -8,7 +12,10 @@ public class MatrixRunner {
 //		new MatrixRunner().testSetRow();
 //		new MatrixRunner().testSetColumn();
 //		new MatrixRunner().testPrint();
-		new MatrixRunner().testSetMatrix();
+//		new MatrixRunner().testSetMatrix();
+//		new MatrixRunner().testCheckSymmetry();
+		new MatrixRunner().testCheckSymmetryMultiDimensional();
+		
 
 	}
 
@@ -54,5 +61,32 @@ public class MatrixRunner {
 		Matrix m = new Matrix(4,5);
 		m.setMatrix(value);
 		m.prettyPrint();
+	}
+	
+	private void testCheckSymmetry() {
+		
+		MatrixChecker m = new MatrixChecker();
+		String msg = m.isSymmetrical(symmetricArray) ? "IS symmetric" : "is NOT symmetric";
+		System.out.println(msg);
+		msg = m.isSymmetrical(asymmetricArray) ? "IS symmetric" : "is NOT symmetric";
+		System.out.println(msg);
+	}
+	
+	private void testCheckSymmetryMultiDimensional() {
+		
+		int[][] symmetricMultiArray = new int[zeroesArray.length][zeroesArray.length];
+		for (int i = 0; i < zeroesArray.length; i++) {
+			symmetricMultiArray[i] = zeroesArray;
+		}	
+		MatrixChecker m = new MatrixChecker();
+		String msg = m.isSymmetrical(symmetricMultiArray) ? "IS symmetric" : "is NOT symmetric";
+		System.out.println(msg);
+		
+		int[][] asymmetricMultiArray = new int[symmetricArray.length][symmetricArray.length];
+		for (int i = 0; i < symmetricArray.length; i++) {
+			asymmetricMultiArray[i] = symmetricArray;
+		}	
+		msg = m.isSymmetrical(asymmetricMultiArray) ? "IS symmetric" : "is NOT symmetric";
+		System.out.println(msg);
 	}
 }
